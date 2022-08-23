9e7c68d262fe76fb9c37e7f4aa8f24517a3e4356
bartosz.molka@gmail.com <74304320+n00sek@users.noreply.github.com>
2022-08-23 14:38:35 +0200

    Added readme

--- api/config/db.go
+++ api/config/db.go
@@ -18,22 +18,19 @@ func Connect() (*gorm.DB, error) {
 	if envError != nil {
 		godotenv.Load()
 	}
-	// host := os.Getenv("DB_HOST")
+	host := os.Getenv("DB_HOST")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
 	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  "host=192.168.144.2 user=" + user + " password=" + password + " dbname=" + dbname + " port=5432 sslmode=disable TimeZone=Europe/Warsaw",
+		DSN:                  "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=5432 sslmode=disable TimeZone=Europe/Warsaw",
 		PreferSimpleProtocol: true, // disables implicit prepared statement usage
 	}), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
 	}
-
-	// dsn := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=5432 sslmode=disable TimeZone=Europe/Warsaw"
-	// db, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	DB = db
 
